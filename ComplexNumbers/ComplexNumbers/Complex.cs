01fb8e74d8911f100780bd8300f0db7d7f634d57
BulatXaberoff <80531785+BulatXaberoff@users.noreply.github.com>
2021-10-22 21:58:53 +0300

    Add files via upload

---
+++ ComplexNumbers/ComplexNumbers/Complex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbers
+{
+    class Complex
+    {
+        public double Re { get; private set; }
+        public double Im { get; private set; }
+
+        private static int count=0;
+        
+        public Complex() { Re = 0;Im = 0; count++; }
+        public Complex(double re,double im)
+        {
+            Re = re;
+            Im = im;
+            count++;
+        }
+        public void Print()
+        {
+            if(Im<0)
+            {
+                Console.WriteLine($"{Re}{Im}i");
+            }
+            Console.WriteLine($"{Re}+{Im}i");
+        }
+        static public Complex operator+(Complex ob1,Complex ob2)
+        {
+            Complex complex = new Complex();
+            complex.Re = ob1.Re + ob2.Re;
+            complex.Im = ob1.Im + ob2.Im;
+            return complex;
+        }
+        static public Complex operator -(Complex ob1, Complex ob2)
+        {
+            Complex complex = new Complex();
+            complex.Re = ob1.Re - ob2.Re;
+            complex.Im = ob1.Im - ob2.Im;
+            return complex;
+        }
+        static public Complex operator *(Complex ob1, Complex ob2)
+        {
+            Complex complex = new Complex();
+            complex.Re = ob1.Re * ob2.Re-(ob1.Im * ob2.Im);
+            complex.Im = ob1.Im*ob2.Re+ob1.Re*ob2.Im;
+            return complex;
+        }
+        static public bool operator==(Complex ob1,Complex ob2)
+        {
+            return (ob1.Im == ob2.Im&&ob1.Re==ob2.Re);
+        }
+        static public bool operator !=(Complex ob1, Complex ob2)
+        {
+            return (ob1.Im != ob2.Im && ob1.Re != ob2.Re);
+        }
+        public override string ToString()
+        {
+            return Im < 0 ? $"{Re}{Im}i" : $"{Re}+{Im}i";
+
+        }
+    }
+}
---
+++ ComplexNumbers/ComplexNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex1 = new Complex(1, 2);
+            complex1.Print();
+            Complex complex2 = new Complex(2, 4);
+            Complex res = new Complex();
+            res = complex1 + complex2;
+            res.ToString();
+            Console.WriteLine(res);
+        }
+    }
+}
